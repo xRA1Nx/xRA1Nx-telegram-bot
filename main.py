85cb9d7b17af17f5c1acc3050de718bbfc06a962
xRA1Nx <stwski@gmail.com>
2022-02-01 10:34:34 +0300

    add new commands

--- main.py
+++ main.py
@@ -1,70 +1,86 @@
 import telebot
 from utills import CheckInput, UserError
 from config import TOKEN
-from data import data_now, weak_weather_fc, d_currency
-
-
+from data import data_now, weak_weather_fc, d_currency, d_massages
 
 bot = telebot.TeleBot(TOKEN)
 
 
 class Start:
-    @staticmethod
-    def now():
+    weather = False
+    exchange = False
+
+    def __init__(self):
+        self.weather = False
+        self.exchange = False
+
+    @classmethod
+    def clear(cls):
+        cls.weather = False
+        cls.exchange = False
+
+    @classmethod
+    def now(cls):
         @bot.message_handler(commands=["start", "help"])
         def info(message: telebot.types.Message):
-            bot.reply_to(message, "для конвертации валюты введите данные в следующем формате\n \
-<валюта 'из'> <валюта 'в'> <кол-во>) \
-\n/currencies - список допустимых валют\
-\n/weather_now - погода в г. Москва сейчас\
-\n/weather_weak - погода в г. Москва на неделю")
+            bot.reply_to(message, d_massages["start"])
 
+        @bot.message_handler(commands=['back'])
+        def back_to_start_menu(message: telebot.types.Message):
+            Start.clear()
+            bot.send_message(message.chat.id, d_massages["start"])
 
+        @bot.message_handler(commands=["exchange"])
+        def exchange(message: telebot.types.Message):
+            bot.send_message(message.chat.id, "\n".join([d_massages["exchange"], d_massages["back"]]))
+            Start.clear()
+            cls.exchange = True
 
         @bot.message_handler(commands=['currencies'])
         def currencies(message: telebot.types.Message):
-            txt = "список допустимых валют:\n"
-            for k in d_currency:
-                txt += f"{k}\n"
-            bot.reply_to(message, txt)
+            if cls.exchange:
+                txt = "список допустимых валют:\n"
+                for k in d_currency:
+                    txt += f"{k}\n"
+                bot.reply_to(message, "\n".join([txt, d_massages['back']]))
+                Start.clear()
+                cls.exchange = True
 
-        @bot.message_handler(commands=['weather_now'])
-        def weather_now(message: telebot.types.Message):
-            bot.reply_to(message, data_now)
-
-        @bot.message_handler(commands=['weather_weak'])
-        def weather_weak(message: telebot.types.Message):
-            bot.send_message(message.chat.id, weak_weather_fc)
-
+        @bot.message_handler(commands=['weather'])
+        def weather(message: telebot.types.Message):
+            bot.reply_to(message, "\n".join([d_massages["weather_intro"], d_massages["back"]]))
+            Start.clear()
+            cls.weather = True
 
         @bot.message_handler(content_types=['text'])
-        def convert(message: telebot.types.Message):
-            try:
-                words = message.text.lower().split()
-                txt = CheckInput.get_price(words)
+        def text_operations(message: telebot.types.Message):
+            if cls.weather:
+                if message.text.lower() not in ["сегодня", "неделя"]:
+                    bot.reply_to(message, f'данные введены не верно\n{d_massages["weather_intro"]}\
+\n{d_massages["back"]}')
+                else:
+                    if message.text.lower() == "сегодня":
+                        bot.send_message(message.chat.id, data_now)
+                    elif message.text.lower() == "неделя":
+                        bot.send_message(message.chat.id, weak_weather_fc)
+                    cls.weather = False
+                    bot.send_message(message.chat.id, d_massages["start"])
 
-            except UserError as e:
-                bot.reply_to(message, f"UserError: {e}")
-            except Exception as e:
-                bot.reply_to(message, f"системный сбой: {e}")
-            else:
-                bot.send_message(message.chat.id, txt)
-
-
-
-
-
+            if cls.exchange:
+                try:
+                    words = message.text.lower().split()
+                    txt = CheckInput.get_price(words)
+                except UserError as e:
+                    bot.reply_to(message, f"UserError: {e}")
+                except Exception as e:
+                    bot.reply_to(message, f"системный сбой: {e}")
+                else:
+                    bot.send_message(message.chat.id, txt)
+                    cls.exchange = False
+                    bot.send_message(message.chat.id, d_massages["start"])
 
         bot.polling(none_stop=True)
 
 
 if __name__ == "__main__":
     Start.now()
-
-
-# @bot.message_handler()
-# def echo(message):
-#     bot.send_message(message.chat.id, "привет")
-
-
-
--- test.py
+++ test.py
@@ -14,8 +14,10 @@ class Start:
         self.weather = False
         self.exchange = False
 
-    def clear(self):
-        self.__init__()
+    @classmethod
+    def clear(cls):
+        cls.weather = False
+        cls.exchange = False
 
     @classmethod
     def now(cls):
@@ -25,49 +27,46 @@ class Start:
 
         @bot.message_handler(commands=['back'])
         def back_to_start_menu(message: telebot.types.Message):
-            Start.weather = False
-            Start.exchange = False
+            Start.clear()
             bot.send_message(message.chat.id, d_massages["start"])
 
         @bot.message_handler(commands=["exchange"])
         def exchange(message: telebot.types.Message):
             bot.send_message(message.chat.id, "\n".join([d_massages["exchange"], d_massages["back"]]))
-            Start.weather = False
-            Start.exchange = True
+            Start.clear()
+            cls.exchange = True
 
         @bot.message_handler(commands=['currencies'])
         def currencies(message: telebot.types.Message):
-            if Start.exchange:
+            if cls.exchange:
                 txt = "список допустимых валют:\n"
                 for k in d_currency:
                     txt += f"{k}\n"
                 bot.reply_to(message, "\n".join([txt, d_massages['back']]))
-                Start.weather = False
-                Start.exchange = True
-
-
+                Start.clear()
+                cls.exchange = True
 
         @bot.message_handler(commands=['weather'])
         def weather(message: telebot.types.Message):
             bot.reply_to(message, "\n".join([d_massages["weather_intro"], d_massages["back"]]))
-            Start.exchange = False
-            Start.weather = True
+            Start.clear()
+            cls.weather = True
 
         @bot.message_handler(content_types=['text'])
         def text_operations(message: telebot.types.Message):
-            if Start.weather:
+            if cls.weather:
                 if message.text.lower() not in ["сегодня", "неделя"]:
                     bot.reply_to(message, f'данные введены не верно\n{d_massages["weather_intro"]}\
 \n{d_massages["back"]}')
                 else:
-                    if message.text == "сегодня":
+                    if message.text.lower() == "сегодня":
                         bot.send_message(message.chat.id, data_now)
-                    elif message.text == "неделя":
+                    elif message.text.lower() == "неделя":
                         bot.send_message(message.chat.id, weak_weather_fc)
-                    Start.weather = False
+                    cls.weather = False
                     bot.send_message(message.chat.id, d_massages["start"])
 
-            if Start.exchange:
+            if cls.exchange:
                 try:
                     words = message.text.lower().split()
                     txt = CheckInput.get_price(words)
@@ -77,16 +76,11 @@ class Start:
                     bot.reply_to(message, f"системный сбой: {e}")
                 else:
                     bot.send_message(message.chat.id, txt)
-                    Start.exchange = False
+                    cls.exchange = False
                     bot.send_message(message.chat.id, d_massages["start"])
-
 
         bot.polling(none_stop=True)
 
 
 if __name__ == "__main__":
     Start.now()
-
-# @bot.message_handler()
-# def echo(message):
-#     bot.send_message(message.chat.id, "привет")
